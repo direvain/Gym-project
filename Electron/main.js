b89c9bbf505664d8310ebb30be4f692aecab4c21
direvain <habboush.abdulrahman@gmail.com>
2025-03-13 16:19:06 +0300

    some config before biuld

--- Electron/main.js
+++ Electron/main.js
@@ -1,13 +1,15 @@
-const { app, BrowserWindow, ipcRenderer } = require('electron');
-const url = require('url');
+const { app, BrowserWindow } = require('electron');
+const { spawn } = require('child_process');
 const path = require('path');
-const { ipcMain } = require('electron');
+const waitOn = require('wait-on');
+
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
-    title: 'My buddy app',
+    title: 'City Gym',
     width: 800,
     height: 600,
+    icon:'./../my-gym/public/images/city_Gym_Icon.png',
     webPreferences: {
       contextIsolation: true,
       nodeIntegration: true,
@@ -19,7 +21,30 @@ function createMainWindow() {
   mainWindow.loadURL('http://localhost:3000');
 }
 
-app.whenReady().then(createMainWindow);
-ipcMain.on('name', (event, arg) => {
-  console.log(arg);
+app.whenReady().then(() => {
+  console.log('Starting backend server...');
+  backendProcess = spawn('npm', ['start'], {
+    cwd: path.join(__dirname, './../backend'),
+    shell: true,
+  });
+
+  backendProcess.stdout.on('data', (data) => console.log(`Backend: ${data}`));
+  backendProcess.stderr.on('data', (data) => console.error(`Backend Error: ${data}`));
+
+  console.log('Starting React frontend...');
+  reactProcess = spawn('npm', ['start'], {
+    cwd: path.join(__dirname, './../my-gym'),
+    shell: true,
+  });
+
+  reactProcess.stdout.on('data', (data) => console.log(`React: ${data}`));
+  reactProcess.stderr.on('data', (data) => console.error(`React Error: ${data}`));
+
+  // Wait for servers to be ready before launching Electron window
+  waitOn({ resources: ['http://localhost:3000'] })
+    .then(() => {
+      console.log('Servers are ready. Launching Electron app...');
+      createMainWindow();
+    })
+    .catch((err) => console.error('Error waiting for servers:', err));
 });
